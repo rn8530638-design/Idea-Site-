84f052fe33259bd6b4caa28cb33f14e00bbf9bb3
RRRwwe <rn8530638@gmail.com>
2025-10-21 16:58:12 +0300

    leson 23

---
+++ backend/src/trpc.ts
@@ -0,0 +1,27 @@
+import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
+
+const ideas = _.times(100, (i) => ({
+  nick: `color-idea-nick-${i}`,
+  name: `idea ${i}`,
+  description: `Description ${i}`,
+  text: _.times(100, (j) => `<p>Text paragrph ${j} of idea ${i}...</p>`).join(''),
+}))
+
+// const ideas = [
+//   { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
+//   { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
+//   { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
+//   { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
+//   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
+// ]
+
+const trpc = initTRPC.create()
+
+export const trpcRouter = trpc.router({
+  getIdeas: trpc.procedure.query(() => {
+    return { ideas: ideas.map((idea) => _.pick(idea, ["nick", "name", "description"])) }
+  }),
+})
+
+export type TrpcRouter = typeof trpcRouter
---
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import { getViewIdeaRoute } from '../../lib/routes'
+import { trpc } from '../../lib/trpc'
+
+export const AllIdeasPage = () => {
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  return (
+    <div>
+      <h1>All Ideas</h1>
+      {data.ideas.map((idea) => (
+        <div key={idea.nick}>
+          <h2>
+            <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>{idea.name}</Link>
+          </h2>
+          <p>{idea.description}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
